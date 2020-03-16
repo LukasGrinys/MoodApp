9c76b3afc0823172fcc5aee2a8344d4b63d9ae8e
LukasGrinys <lukas.grinys@gmail.com>
2020-03-16 21:41:16 +0200

    Final commit

--- server/server.js
+++ server/server.js
@@ -23,11 +23,11 @@ app.use(express.static(__dirname + '/public'));
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
     app.use(express.static('client/build'));
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // // Express serve up index.html file if it doesn't recognize route
+    // const path = require('path');
+    // app.get('*', (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 // API routes
 // DEMO get users

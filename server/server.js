fd9d62761a64bb05e9b88eae9ca7990cdf74ebaa
LukasGrinys <lukas.grinys@gmail.com>
2020-03-16 21:21:25 +0200

    Final commit

--- server/server.js
+++ server/server.js
@@ -20,6 +20,15 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 // API routes
 // DEMO get users
 app.get('/api/users', (req,res) => {

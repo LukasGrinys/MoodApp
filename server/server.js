d9e33ca5a51d8f7f9bd3310f1275f1a2aa538dac
LukasGrinys <lukas.grinys@gmail.com>
2020-03-18 19:31:05 +0200

    lala

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/public'))
 
 if (process.env.NODE_ENV === 'production') {    
-    app.use(express.static(__dirname + '/../client/build'));
+    // app.use(express.static(__dirname + '/../client/build'));
     app.get('/*', function(req, res) {
         res.sendFile(path.join(__dirname, '/../client/build/index.html'), function(err) {
           if (err) {

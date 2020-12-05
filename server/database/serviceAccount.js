c4ee49158e0f1801ee37a89211f902e07fa933c0
LukasGrinys <lukas.grinys@gmail.com>
2020-12-05 12:44:35 +0200

    Firebase [config] Fix the private key for proper parsing

--- server/database/serviceAccount.js
+++ server/database/serviceAccount.js
@@ -2,7 +2,7 @@ const serviceAccount = {
     "type": "service_account",
     "project_id": "moodapp-f7267",
     "private_key_id": process.env.PRIVATE_DB_KEY_ID,
-    "private_key": process.env.PRIVATE_DB_KEY,
+    "private_key": process.env.PRIVATE_DB_KEY.replace(/\\n/g, '\n'),
     "client_email": "firebase-adminsdk-q04n2@moodapp-f7267.iam.gserviceaccount.com",
     "client_id": "101915873275470025652",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",

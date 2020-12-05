a32f6682a083317767b0177cd9989963b75e7025
LukasGrinys <lukas.grinys@gmail.com>
2020-12-05 12:52:15 +0200

    Bring back build folder

---
+++ client/build/precache-manifest.d0939ae9136dce8e24b549a7a24164f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892651d347c2361d3f950dfcabeae566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef2fa13e8fdb4ca9fb6",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "cfbac6aef32b5dc473bf",
+    "url": "/static/css/main.292834ea.chunk.css"
+  },
+  {
+    "revision": "4ef2fa13e8fdb4ca9fb6",
+    "url": "/static/js/2.b8112460.chunk.js"
+  },
+  {
+    "revision": "66b128007e485ad3f41e7ba631ec1573",
+    "url": "/static/js/2.b8112460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfbac6aef32b5dc473bf",
+    "url": "/static/js/main.5291a221.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "1bf5e1d29dbb28f926a8ff3abfa8d1ef",
+    "url": "/static/media/afternoon.1bf5e1d2.svg"
+  },
+  {
+    "revision": "30a34177e9cf9e8f2e3cd813ffebdaf6",
+    "url": "/static/media/evening.30a34177.svg"
+  },
+  {
+    "revision": "32026445892ef627687f6bea064b2049",
+    "url": "/static/media/morning.32026445.svg"
+  }
+]);
